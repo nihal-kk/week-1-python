98aaac08143a8acd72bf2e6f45f5eb02a6fa6896
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-23 15:56:10 +0530

    Create Numeric Types.py

---
+++ Numeric Types.py
@@ -0,0 +1,17 @@
+#>Numeric Types 
+
+number =190  #integer
+number1 =18.5    #flout
+number2 = 20j       #complex
+
+print(number)
+print(type(number))
+
+print(number1)
+print(type(number1))
+
+print(number2)
+print(type(number2))
+
+print(number+number1)
+print(number,number1,number2)
