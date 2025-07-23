5cfd9f45584a3caa5d459d9eca3f501cbee92621
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-07-23 16:03:19 +0530

    Create sequence Types.py

---
+++ sequence Types.py
@@ -0,0 +1,15 @@
+#>sequence Types
+
+names = ["john","jomon","jery"]  #list
+print (names)
+print(names[0])
+print(names[1])
+print(names[2])
+
+products =("vivo","iphone","oppo","realme")  #tuple
+
+print(products)
+print(products[1])
+
+
+
